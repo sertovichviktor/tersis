e79c0320a943c07ac01aed6f16887df26f8287d5
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-04-29 19:45:13 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,13 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-// Создаем роутер для SPA (без серверных функций)
 export const router = createRouter({
   routeTree,
+  // ЭТО ЗАСТАВИТ САЙТ ЗАПОМИНАТЬ ПОЗИЦИЮ СКРОЛЛА
+  scrollRestoration: true, 
   defaultPreload: 'intent',
 })
 
-// Регистрируем роутер для поддержки типов
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
