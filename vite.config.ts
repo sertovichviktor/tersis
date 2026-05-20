4bbfc507a5fc12aff3ee051aebc63d3d2711264f
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 17:27:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Используем встроенный esbuild, он точно есть и он быстрее
-    minify: 'esbuild',
-    sourcemap: false
-  }
+  plugins: [react()]
 })
