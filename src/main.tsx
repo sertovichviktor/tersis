e5dd2524cc44234668ebf9edd66bee15b7a4ccee
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 17:28:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './style.css'
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <RouterProvider router={router} />
-  )
+  root.render(<RouterProvider router={router} />)
 }
