beea5b4ca17d75fa9321648c4dc23b69be904d33
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 18:24:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './style.css'
+import './styles.css' // <-- ДОБАВИЛ БУКВУ S, ТЕПЕРЬ ЗАБИЛДИТСЯ
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <RouterProvider router={router} />
-  )
+  root.render(<RouterProvider router={router} />)
 }
