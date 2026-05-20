bb2097d022ed8bb5e2cbcc87419a85caf0492403
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 17:05:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    sourcemap: false, // ВЫКЛЮЧИ ЭТО, чтобы файлы были легче
-    minify: 'terser', // Хорошее сжатие
+    sourcemap: false,
+    minify: 'terser',
   }
 })
