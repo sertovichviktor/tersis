3e7f7bc54e9f6e5b1fb7b1fdbdbd847734d2f1d8
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 17:13:55 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './style.css' // Проверь: style.css или styles.css?
+import './style.css'
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
-    // Убрали StrictMode для снижения нагрузки на Fornex
     <RouterProvider router={router} />
   )
 }
