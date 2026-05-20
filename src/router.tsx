fd87356ab9a9325efa79ee4123570820883b681f
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 19:44:50 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,7 @@ import { routeTree } from './routeTree.gen'
 
 export const router = createRouter({
   routeTree,
-  // ВЫКЛЮЧАЕМ ПРИЧИНЫ ФРИЗА:
+  // ВЫКЛЮЧАЕМ виновников зависания:
   scrollRestoration: false, 
   defaultPreload: false,
 })
