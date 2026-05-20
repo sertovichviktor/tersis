334ef505db608038860525d74829c5385217e536
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 16:54:06 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,7 @@ const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+    // МЫ УБРАЛИ StrictMode — это уберет лишнюю нагрузку при кликах
+    <RouterProvider router={router} />
   )
 }
