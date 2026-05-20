853222578b28da0f2e1a535bee52fe554ca1ce1e
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-20 20:11:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    sourcemap: false,
-    minify: 'esbuild',
-  }
+  plugins: [react()],
+  base: '/',
 })
