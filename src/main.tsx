1ec7982c2f11f82325c17d88edfa3562a928057f
sertovichviktor <142357565+sertovichviktor@users.noreply.github.com>
2026-05-21 15:15:20 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Мы импортируем файл напрямую. Проверь, чтобы он назывался App.tsx (с большой буквы)
 import App from './App' 
 import './styles.css'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <App />
-  )
+  root.render(<App />)
 }
